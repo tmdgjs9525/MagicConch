d1f36875e2e810ba621e2a49f9fca3f0ddcfac11
seungheon <tmdgjs9525@gmail.com>
2025-02-13 17:20:11 +0900

    NAudio 추가/ .netStandard 2.0에서 .net 9.0 변경

---
+++ MagicConch/MagicConch/Helper/SoundHelper.cs
@@ -0,0 +1,50 @@
+﻿using NAudio.Wave;
+using System;
+namespace MagicConch.Helper
+{
+    internal class SoundHelper
+    {
+        string audioFilePath;
+
+        AudioFileReader audioFile;
+        WaveOutEvent outputDevice;
+        public SoundHelper(string filePath)
+        {
+            audioFilePath = filePath;
+            audioFile = new AudioFileReader(audioFilePath);
+            outputDevice = new WaveOutEvent();
+
+            // 초기 필수
+            outputDevice.Init(audioFile);
+        }
+        public void Play()
+        {
+            outputDevice.Play();
+        }
+        public void PlayLooping()
+        {
+            outputDevice.PlaybackStopped += PlaybackStoppedHandler!;
+            outputDevice.Play();
+        }
+        public void Stop()
+        {
+            outputDevice.PlaybackStopped -= PlaybackStoppedHandler!;
+            outputDevice.Stop();
+        }
+        public void SetVolume(float volume)
+        {
+            outputDevice.Volume = volume;
+        }
+        public void Dispose()
+        {
+            outputDevice.Dispose();
+            audioFile.Dispose();
+        }
+        public void PlaybackStoppedHandler(object sender, StoppedEventArgs e)
+        {
+            // 오디오 파일의 위치를 처음으로 재설정
+            audioFile.Position = 0;
+            outputDevice.Play();
+        }
+    }
+}
