dbe8fbca4b4d58b8f715367fa28a5806964714fa
seungheon <tmdgjs9525@gmail.com>
2025-02-18 10:23:18 +0900

    converter, stringformat으로 Sound: On

---
+++ MagicConch/MagicConch/Converter/BoolToOnOffConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MagicConch.Converter
+{
+    internal class BoolToOnOffConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? "On" : "Off";
+            }
+
+            Debug.Assert(false);
+            return "Binding Type is not Boolean";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
