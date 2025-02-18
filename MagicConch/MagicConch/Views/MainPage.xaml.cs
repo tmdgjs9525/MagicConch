ce3b443a81c12f5bfc0ee224910e629e56b8a159
seungheon <tmdgjs9525@gmail.com>
2025-02-18 12:27:03 +0900

    컨트롤 + A 차단

--- MagicConch/MagicConch/Views/MainPage.xaml.cs
+++ MagicConch/MagicConch/Views/MainPage.xaml.cs
@@ -1,17 +1,32 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace MagicConch.Views
 {
     public partial class MainPage : Page
     {
+        private DateTime lastClickTime = DateTime.MinValue;
+        private int clickCount = 0;
+        private readonly TimeSpan doubleClickThreshold = TimeSpan.FromMilliseconds(700);
+
         public MainPage()
         {
             this.InitializeComponent();
 
             MouseRightButtonDown += MainPage_MouseRightButtonDown;
+            KeyDown += MainPage_KeyDown;
         }
 
-        private void MainPage_MouseRightButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void MainPage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.A && (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
+            { // Ctrl + A 입력을 차단
+                e.Handled = true; 
+            }
+        }
+
+        private void MainPage_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
             e.Handled = true;
         }
